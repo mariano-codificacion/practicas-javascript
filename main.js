0b166073259a27c3b4ff8c4b3facd9e7f6d64d58
mtoranzo <marianoeto@hotmail.com>
2023-04-19 18:42:20 -0300

    se agregan sumas y restas

--- main.js
+++ main.js
@@ -15,14 +15,15 @@ class Operacion {
             let s = 0;
             listaNrosVacia.splice (0,listaNrosVacia.length);
             for (let i = 1; i <= valores; i++) {
-                const rndInt = randomIntFromInterval(1, 6);
+                let rndInt = randomIntFromInterval(-6, 6);
                 listaNrosVacia.push (rndInt);
                 console.log(rndInt);
                 alert("Sumar: " + rndInt);
                 s = s + rndInt;
             }
-            let listaString = listaNrosVacia.join("+");
-            alert("Los Numeros Son: " + listaString);
+            listaNrosVacia.sort((a,b)=> b - a);
+            let listaString = listaNrosVacia.join(")+(");
+            alert("Los Numeros Son: (" + listaString + ")");
             let resultado = parseInt(prompt("Ingrese el resultado de la suma de los numeros mostrados:"));
             if (resultado == s) {
                 alert("Correcto El Resultado es: " + s);
@@ -39,7 +40,7 @@ class Operacion {
             let s = 1;
             listaNrosVacia.splice (0,listaNrosVacia.length);
             for (let i = 1; i <= valores; i++) {
-                const rndInt = randomIntFromInterval(1, 6);
+                let rndInt = randomIntFromInterval(1, 6);
                 listaNrosVacia.push (rndInt);
                 console.log(rndInt);
                 alert("Los Numeros Son: " + rndInt);
@@ -58,18 +59,17 @@ class Operacion {
     }
 }
 function operaciones() {
-    operar = prompt("¿Desea realizar calculos de sumas o multiplicaciones de Nros Naturales: (Si o No)?");
+    operar = prompt("¿Desea realizar calculos de sumas y restas o multiplicaciones de Nros Naturales: (Si o No)?");
     if (operar != "NO" && operar != "no" && operar != null) {
         valores = parseInt(prompt("¿Cuántos valores Quieres Calcular?(Entre 2 y 10):"));
-        n = prompt("Elige la operacion: (1)Suma. (2)Multiplicación. (3)Salir.")
+        n = prompt("Elige la operacion: \n (1)Sumas y Restas \n (2)Multiplicación. \n (3)Salir")
         if (n == 1) {
-            const suma = new Operacion(valores);
+             const suma = new Operacion(valores);
             suma.sumar();
         } else if (n == 2) {
             const multiplicacion = new Operacion(valores);
             multiplicacion.multiplicar();
-        } else if (n == 3){
-
+        } else if (n == 3) {
         }
       
     }
