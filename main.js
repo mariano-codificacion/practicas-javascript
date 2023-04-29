ea400c15d9d23b254457d729203cdd24d32cecda
mtoranzo <marianoeto@hotmail.com>
2023-04-29 17:52:09 -0300

    dado y mapa

--- main.js
+++ main.js
@@ -5,57 +5,73 @@ let operar = null;
 let n = 0;
 let listaNrosVacia = [];
 let rndInt;
+
+function randomIntFromInterval(min, max) { // min and max included 
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
 class dado {
     constructor(valor, url) {
         this.valor = valor;
         this.url = url;
     }
 
-    avanzar() {
+    crearDado() {
         const contenedor = document.getElementById("contenedor");
+        let rndInt = randomIntFromInterval(1, 6);
+        console.log(rndInt);
         if (rndInt == 1) {
             const div = document.createElement("div");
             div.className = "caja";
             div.innerHTML = `<p class="textDado"> AVANZA: ${dado1.valor} </p>
                         <img class="dados" src="${dado1.url}"
-                         alt= "${dado1.valor}">`
+                         alt= "${dado1.valor}">
+                         <button> Tirar Dado </button>`;
             contenedor.appendChild(div);
         } else if (rndInt == 2) {
             const div = document.createElement("div");
             div.className = "caja";
             div.innerHTML = `<p class="textDado"> AVANZA: ${dado2.valor} </p>
                         <img class="dados" src="${dado2.url}"
-                        alt= "${dado2.valor}">`
+                        alt= "${dado2.valor}">
+                        <button> Tirar Dado </button>`;
             contenedor.appendChild(div);
         } else if (rndInt == 3) {
             const div = document.createElement("div");
             div.className = "caja";
             div.innerHTML = `<p class="textDado"> AVANZA: ${dado3.valor} </p>
                         <img class="dados" src="${dado3.url}"
-                        alt= "${dado3.valor}">`
+                        alt= "${dado3.valor}">
+                        <button> Tirar Dado </button>`;
             contenedor.appendChild(div);
         } else if (rndInt == 4) {
             const div = document.createElement("div");
             div.className = "caja";
             div.innerHTML = `<p class="textDado"> AVANZA: ${dado4.valor} </p>
                         <img class="dados" src="${dado4.url}"
-                        alt= "${dado4.valor}">`
+                        alt= "${dado4.valor}">
+                        <button> Tirar Dado </button>`;
             contenedor.appendChild(div);
         } else if (rndInt == 5) {
             const div = document.createElement("div");
             div.className = "caja";
             div.innerHTML = `<p class="textDado"> AVANZA: ${dado5.valor} </p>
                         <img class="dados" src="${dado5.url}"
-                        alt= "${dado5.valor}">`
+                        alt= "${dado5.valor}">
+                        <button> Tirar Dado </button>`;
             contenedor.appendChild(div);
         } else {
             const div = document.createElement("div");
             div.className = "caja";
             div.innerHTML = `<p class="textDado"> AVANZA: ${dado6.valor} </p>
                         <img class="dados" src="${dado6.url}"
-                        alt= "${dado6.valor}">`
+                        alt= "${dado6.valor}">
+                        <button> Tirar Dado </button>`;
             contenedor.appendChild(div);
         }
+    }
+    tirarDado() {
+    contenedor.innerHTML = ``;
+    div.crearDado();
     }
 }
 const dado1 = new dado(1, "img/dado-animado-1.gif");
@@ -64,7 +80,31 @@ const dado3 = new dado(3, "img/dado-animado-3.gif");
 const dado4 = new dado(4, "img/dado-animado-4.gif");
 const dado5 = new dado(5, "img/dado-animado-5.gif");
 const dado6 = new dado(6, "img/dado-animado-6.gif");
-const dados = new dado();
+const div = new dado();
+const mapa = new dado(0,"img/Mapa_Escuela.jpg");
+
+function titulo (){
+const pie = document.getElementById("pie");
+const parrafo = document.createElement("p");
+parrafo.innerText = "Este es el camino a la Escuela"; 
+parrafo.className = "tinki";
+pie.appendChild(parrafo);
+}
+
+function tiraDado (){
+let tirar = prompt("¿Deseas Tirar el Dado? (Si o No)?");
+div.tirarDado();
+}
+
+function mapaEscuela (){
+const contenedor1 = document.getElementById("contenedor1");
+const div1 = document.createElement("div");
+div1.className = "caja1";
+div1.innerHTML = `<p class="textImagen"> Llegamos Jugando a la Escuela: </p>
+            <img class="imgMapa" src="${mapa.url}"
+            alt= "${mapa.url}">`;
+contenedor1.appendChild(div1);
+}
 
 class Operacion {
     constructor(valores) {
@@ -92,7 +132,7 @@ class Operacion {
             } else {
                 alert("Incorrecto El Resultado es: " + s);
             }
-            operaciones();
+            
         }
     }
 
@@ -103,7 +143,6 @@ class Operacion {
             listaNrosVacia.splice(0, listaNrosVacia.length);
             for (let i = 1; i <= valores; i++) {
                 rndInt = randomIntFromInterval(1, 6);
-                dados.avanzar()
                 listaNrosVacia.push(rndInt);
                 alert("Los Numeros Son: " + rndInt);
                 s = s * rndInt;
@@ -116,28 +155,12 @@ class Operacion {
             } else {
                 alert("Incorrecto El Resultado es: " + s);
             }
-            operaciones();
+            
         }
     }
 }
-function operaciones() {
-    operar = prompt("¿Desea realizar calculos de sumas y restas o multiplicaciones de Nros Naturales: (Si o No)?");
-    if (operar != "NO" && operar != "no" && operar != null) {
-        valores = parseInt(prompt("¿Cuántos valores Quieres Calcular?(Entre 2 y 10):"));
-        n = prompt("Elige la operacion: \n (1)Sumas y Restas \n (2)Multiplicación. \n (3)Salir")
-        if (n == 1) {
-            const suma = new Operacion(valores);
-            suma.sumar();
-        } else if (n == 2) {
-            const multiplicacion = new Operacion(valores);
-            multiplicacion.multiplicar();
-        } else if (n == 3) {
-        }
+titulo ()
+tiraDado()
+mapaEscuela()
 
-    }
-    return (operar, valores);
-}
-function randomIntFromInterval(min, max) { // min and max included 
-    return Math.floor(Math.random() * (max - min + 1) + min)
-}
-operaciones();
+
